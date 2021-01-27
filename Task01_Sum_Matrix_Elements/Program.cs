4466f6d2523068a011a623d168da671c96b83471
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-27 19:38:03 +0200

    Add files via upload

---
+++ Task01_Sum_Matrix_Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Task01_Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = size[0];
+            int colums = size[1];
+
+            int[,] matrix = new int [rows, colums];
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elements = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                
+                for (int colum = 0; colum < matrix.GetLength(1); colum++)
+                {
+                    matrix[row, colum] = elements[colum];
+                    sum += elements[colum];
+                }
+            }
+            
+            Console.WriteLine(rows);
+            Console.WriteLine(colums);
+            Console.WriteLine(sum);
+        }
+    }
+}
